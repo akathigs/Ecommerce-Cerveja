1ad7d274d572bc32a6cdd96f0d00328fad32ad3d
akathigs <thiagoawk@gmail.com>
2022-10-25 23:03:22 -0300

    models update

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class produto(models.Model):
+    nameProd = models.CharField(max_length=30)
+    values = models.FloatField(max_length=30)
+    mls = models.models.FloatField(max_length=5)
+
+class user(models.Model):
+    login = models.CharField(max_length=20)
+    password = models.CharField(max_length=30)
+    username = models.FloatField(max_length=30)
